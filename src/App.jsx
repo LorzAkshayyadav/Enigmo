e03e0f1a991cad107d23fff01f1b09de0fbda546
LorzAkshayyadav <ee21b012@smail.iitm.ac.in>
2025-03-27 12:37:29 +0530

    Some edit

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import { useState, useEffect, useRef } from "react";
 import { motion, AnimatePresence } from "framer-motion";
+import { Font } from "three/examples/jsm/Addons.js";
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
   const dropdownRef = useRef(null);
 
-  const instruments = ["Guitar", "Piano", "Violin", "Drums", "Flute", "Saxophone", "Trumpet", "Cello", "Harmonica"];
-
+  const instruments = ["Scissor","Grasper","Holder","Disector","Teneculum"];
+  
   useEffect(() => {
     function handleClickOutside(event) {
       if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
@@ -25,18 +26,15 @@ function App() {
 
   return (
     <>
-      {/* Toolbar */}
       <div className="Tool-Bar">
         <button className="graph1">Simulation Mode</button>
         <button className="graph1">Hardware Mode</button>
 
-        {/* Dropdown Button */}
+
         <div className="relative" ref={dropdownRef}>
           <button className="graph1 menu-bar" onClick={() => setIsOpen(!isOpen)}>
             Instrument Type ▼
           </button>
-
-          {/* Dropdown Menu */}
           <AnimatePresence>
             {isOpen && (
               <motion.div
@@ -65,23 +63,35 @@ function App() {
 
         <button className="graph">Plot Graph</button>
       </div>
-
-      {/* Main Content */}
       <div className="App">
+      <div className="wer1">
+        <div>
         <span className="heading">
           <label className="switch">
             <input type="checkbox" />
             <span className="slider round"></span>
           </label>
-          <h3>Switch:</h3>
+          <span className="bold">Switch :</span>
         </span>
-
+        </div>
+        <div>
         <Canvas camera={{ fov: 50, position: [-6, 6, 6] }}>
           <ambientLight intensity={7} />
           <OrbitControls enableZoom={true} />
           <Model />
         </Canvas>
+        </div>
+        </div>
+        <div className="wer">
+        <div>
+          <h2>Actuators :</h2>
+        </div>
+        <div>
+          <h2>Instruments :</h2>
+        </div>
       </div>
+      </div>
+     
     </>
   );
 }
